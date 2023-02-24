152a55259f03c4c175405d7b696523beaba96575
lalva224 <alvarezleandro720@gmail.com>
2023-02-24 12:38:03 -0500

    first commit

---
+++ Library_Project/src/main/java/Library_Project/Library_Project/LibraryProjectApplication.java
@@ -0,0 +1,13 @@
+package Library_Project.Library_Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibraryProjectApplication.class, args);
+	}
+
+}
---
+++ Library_Project/src/test/java/Library_Project/Library_Project/LibraryProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package Library_Project.Library_Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
